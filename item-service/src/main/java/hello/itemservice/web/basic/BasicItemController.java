a8db2e30797dff7e4754a0fd6f23879ee6d95428
alex0317@naver.com <joohanpark1513@gmail.com>
2023-01-22 21:44:44 +0900

    백엔드 공부 - 상품 등록 처리(@ModelAttribute)

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -71,9 +71,26 @@ public class BasicItemController {
         return "basic/item";
     }
 
+
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    // 상품 수정 폼 보여주기
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
+    // 상품 수정, 상품 저장
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     // 테스트용 데이터 추가
