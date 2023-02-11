813ea284ca1f99cef2b5b08b97d981391f0678c0
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-11 11:01:07 +0900

    백엔드 공부

---
+++ TestProjet/src/main/java/com/example/TestProjet/TestProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjetApplication.class, args);
+	}
+
+}
