ab5224fae76c944ef2caaa9be2c111635f752f1f
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-23 18:24:36 +0900

    백엔드 학습 - 유저 생성 API

--- library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
+++ library-app/src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
@@ -5,11 +5,6 @@ public class CalculatorMultiplyRequest {
     private int num1;
     private int num2;
 
-    public CalculatorMultiplyRequest(int num1, int num2) {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
-
     public int getNum1() {
         return num1;
     }
