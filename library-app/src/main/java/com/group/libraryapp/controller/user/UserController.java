ec36b2cccb03c98d3ffc44945760e8ae8bd519d5
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-24 09:24:48 +0900

    백엔드 학습 - Controller에서 Service, Repository 분리

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,79 +1,40 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final UserService userService;
 
     public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.userService = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user")   //POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
-//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-//            @Override
-//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-//                long id = rs.getLong("id");
-//                String name = rs.getString("name");
-//                int age = rs.getInt("age");
-//                return new UserResponse(id, name, age);
-//            }
-//        });
-        // lamda 방식
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        String readSql = "SELECT * FROM user WHERE id = ?";
-        // request.getId가 ?에 들어가게 되고 SELECT SQL 결과가 있으면 0으로 변환 (최종적으로 List로 반환)
-        // 결론적으로 해당 id를 가진 유저가 있으면 0이 담긴 List가 반환
-        // 없다면 빈 List가 반환
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-        // 유저가 존재하지 않을 때 오루 발생
-        if(isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        String readSql = "SELECT * FROM user WHERE name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-        if(isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 }
---
+++ library-app/src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,63 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void saveUser(String name, Integer age) {
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, name, age);
+    }
+
+    public List<UserResponse> getUsers() {
+        String sql = "SELECT * FROM user";
+//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//            @Override
+//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                long id = rs.getLong("id");
+//                String name = rs.getString("name");
+//                int age = rs.getInt("age");
+//                return new UserResponse(id, name, age);
+//            }
+//        });
+        // lamda 방식
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    public void updateUserName( String name, long id) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    public boolean isUserNotExist(String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+
+    public boolean isUserNotExist(long id) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        // request.getId가 ?에 들어가게 되고 SELECT SQL 결과가 있으면 0으로 변환 (최종적으로 List로 반환)
+        // 결론적으로 해당 id를 가진 유저가 있으면 0이 담긴 List가 반환
+        // 없다면 빈 List가 반환
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    public void deleteUser(String name) {
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+}
