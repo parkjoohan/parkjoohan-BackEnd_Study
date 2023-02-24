32fb4065991580a460fc9f78b6414dfe12bfd0c8
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-24 12:01:49 +0900

    백엔드 학습 - 스프링 컨테이너 다루기

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,6 +4,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,11 +12,24 @@ import java.util.List;
 @RestController
 public class UserController {
 
+//    생성자를 사용하여 스프링 빈 주입
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+//    setter와 @Autowired를 사용한 스프링 빈 주입
+//    private UserService userService;
+//
+//    @Autowired
+//    public void setUserService(UserService userService) {
+//        this.userService = userService;
+//    }
+
+//    @Autowired만 사용하여 스프링 빈 주입
+//    @Autowired
+//    private UserService userService;
 
     @PostMapping("/user")   //POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
