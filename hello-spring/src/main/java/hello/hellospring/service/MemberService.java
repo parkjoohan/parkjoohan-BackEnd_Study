bd55ce7deb1d3c6088e5ba5d0afe52dee0844fd6
alex0317@naver.com <joohanpark1513@gmail.com>
2023-03-27 11:26:11 +0900

    BackEnd 공부

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -19,10 +21,20 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member);    // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            // 같은 이름이 있는 중복 회원x
+            validateDuplicateMember(member);    // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
