3678f723fc25fd9c42821345556c0df05d8472ae
alex0317@naver.com <joohanpark1513@gmail.com>
2023-03-28 22:55:29 +0900

    BackEnd 공부

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -21,14 +21,14 @@ public class RateDiscountPolicyTest {
         Assertions.assertThat(discount).isEqualTo(1000);
     }
 
-    @Test
-    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
-    void vip_x() {
-        // given
-        Member member = new Member(2L, "memberBASIC", Grade.VIP);
-        // when
-        int discount = discountPolicy.discount(member, 10000);
-        // then
-        Assertions.assertThat(discount).isEqualTo(0);
-    }
+//    @Test
+//    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
+//    void vip_x() {
+//        // given
+//        Member member = new Member(2L, "memberBASIC", Grade.VIP);
+//        // when
+//        int discount = discountPolicy.discount(member, 10000);
+//        // then
+//        Assertions.assertThat(discount).isEqualTo(0);
+//    }
 }
