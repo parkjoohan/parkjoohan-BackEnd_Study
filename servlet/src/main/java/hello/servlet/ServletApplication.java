685ee928a752e6db562dbd6519623b81510128d9
alex0317@naver.com <joohanpark1513@gmail.com>
2023-04-02 14:28:09 +0900

    BackEnd 공부

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,12 +2,6 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-@ServletComponentScan	// 서블릿 자동 등록
 
 @SpringBootApplication
 public class ServletApplication {
@@ -16,9 +10,4 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
-	// ViewResolver가 나타나기 전에 사용되던 방식
-	//	@Bean
-	//	ViewResolver internalResourceViewResolver() {
-	//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
-	//	}
 }
