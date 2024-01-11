0d0e137aed73644961e82ba84aaaeb9c197a7a48
parkjoohan <joohanpark1513@gmail.com>
2024-01-11 09:42:46 +0900

    UPDATE(2024.01.10) - React useEffect

--- FrontEnd/react-beginners/src/App.js
+++ FrontEnd/react-beginners/src/App.js
@@ -1,11 +1,27 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5)
+      console.log("SEARCH FOR ", keyword);
+  }, [keyword]);
+
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
